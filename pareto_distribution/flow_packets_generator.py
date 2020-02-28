343177edcb0923cffccf15639be415b9a073d661
x-bin <842804906@qq.com>
2020-02-28 13:41:06 +0800

    third version of flow packets generator

--- pareto_distribution/flow_packets_generator.py
+++ pareto_distribution/flow_packets_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import bisect
+import time
 class flow_packets_generator:
     def __init__(self, concurrency_flow_nums, pkts_num, min_num, alpha, save_file):
         self.concurrency_flow_nums = concurrency_flow_nums
@@ -19,7 +20,8 @@ class flow_packets_generator:
             self.flowid_record.append(tempid)
 
         self.pkts_sequence = []
-        self.get_pkts_sequence()
+        #self.get_pkts_sequence()
+        self.new_get_pkts_sequence()
 
     def show_flow_distribution(self):
         sorted_distribution = np.sort(self.flow_distribution_record)
@@ -43,10 +45,13 @@ class flow_packets_generator:
         pkts_num_now = sum(self.flow_distribution)
         with open(self.save_file,'w') as f:
             f.write("[")
-
+            st_time = time.time()
+            flag_time = st_time
             for i in range(self.pkts_num):
-                if i % 1000000 == 0:
-                    print("进度报告：", i / self.pkts_num)
+                if i % 10000 == 0:
+                    time_now = time.time()
+                    print("进度报告：", i, "单位时间", time_now - st_time, "总时间：", time_now - flag_time)
+                    st_time = time_now
                 temp_index = np.random.randint(0, pkts_num_now)
                 add_index = 0
                 stop_index = 0
@@ -60,19 +65,59 @@ class flow_packets_generator:
                 else:
                     pkt = self.flowid[stop_index]
                 f.write(pkt)
-                self.pkts_sequence.append(pkt)
-                pkts_num_now -= 1
-                self.flow_distribution[stop_index] -= 1
-                if self.flow_distribution[stop_index] == 0:
+                if self.flow_distribution[stop_index] == 1:
                     new_flow_num = (np.floor((np.random.pareto(self.alpha, size=1) + 1) * self.min_num).astype(int))[0]
                     self.flow_distribution[stop_index] = new_flow_num
                     new_flow_id = self.get_rand_flowid()
                     self.flowid[stop_index] = new_flow_id
-                    self.flowid_record.append(new_flow_id)
-                    pkts_num_now += new_flow_num
+                    pkts_num_now += (new_flow_num - 1)
+                else:
+                    pkts_num_now -= 1
+                    self.flow_distribution[stop_index] -= 1
             f.write("]")
-            print("最终生成的总流个数：", len(self.flowid_record))
-            print("平均每个流的数据包数量为：", len(self.flowid_record) / self.pkts_num)
+
+    def new_get_pkts_sequence(self):
+        pkts_num_now = sum(self.flow_distribution)
+        with open(self.save_file, 'w') as f:
+            f.write("[")
+            prefix_sum = []
+            tmp_sum = 0
+            st_time = time.time()
+            flag_time = st_time
+            for i in range(self.concurrency_flow_nums):
+                tmp_sum += self.flow_distribution[i]
+                prefix_sum.append(tmp_sum)
+            prefix_sum = np.array(prefix_sum).astype(int)
+            for i in range(self.pkts_num):
+                if i % 10000 == 0:
+                    time_now = time.time()
+                    print("进度报告：", i, "单位时间", time_now - st_time, "总时间：", time_now - flag_time)
+                    st_time = time_now
+                temp_index = np.random.randint(0, pkts_num_now)
+                stop_index = bisect.bisect_right(prefix_sum, temp_index)
+                if i != self.pkts_num - 1:
+                    pkt = self.flowid[stop_index] + ","
+                else:
+                    pkt = self.flowid[stop_index]
+                f.write(pkt)
+                if self.flow_distribution[stop_index] == 1:
+                    new_flow_num = (np.floor((np.random.pareto(self.alpha, size=1) + 1) * self.min_num).astype(int))[0]
+                    self.flow_distribution[stop_index] = new_flow_num
+                    new_flow_id = self.get_rand_flowid()
+                    self.flowid[stop_index] = new_flow_id
+                    pkts_num_now += (new_flow_num-1)
+                    st = np.zeros(stop_index, dtype=int)
+                    ed = np.ones(self.concurrency_flow_nums - stop_index, dtype=int)
+                    temp_num = np.concatenate((st, ed))
+                    prefix_sum = prefix_sum + (new_flow_num - 1) * temp_num
+                else:
+                    st = np.zeros(stop_index, dtype=int)
+                    ed = np.ones(self.concurrency_flow_nums - stop_index, dtype=int)
+                    temp_num = np.concatenate((st, ed))
+                    prefix_sum = prefix_sum - temp_num
+                    pkts_num_now -= 1
+                    self.flow_distribution[stop_index] -= 1
+            f.write("]")
 
     def get_rand_flowid(self):
         lst = []
@@ -93,7 +138,7 @@ class flow_packets_generator:
 
 
 if __name__ == "__main__":
-    for i in range(1, 6):
+    for i in range(2, 6):
         print("第", i, "次 trace 文件开始生成")
         filename = "trace" + str(i)
         ge = flow_packets_generator(100000, 20010000, 1, 1 + i * 0.05, filename)
